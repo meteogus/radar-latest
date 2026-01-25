ddc3a4598857ab75cddaa9229c4da15ce69bf499
meteogus <loutaskon@gmail.com>
2026-01-25 21:55:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cron = require('node-cron');
 const app = express();
 const PORT = process.env.PORT || 10000;
 const IMAGE_PATH = 'radar-latest.png';
-const CROP_BOTTOM_PX = 60;
+const CROP_BOTTOM_PX = 70;
 
 async function fetchRadar() {
     try {
@@ -112,5 +112,6 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     fetchRadar(); // initial fetch
 });
+
 
 
